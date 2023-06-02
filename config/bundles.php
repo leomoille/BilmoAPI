82cb160995818f39de6c6f7d5405d70052360f13
Léo Moille <leomoille18@gmail.com>
2023-06-02 13:19:20 +0200

    :package: Add API Platform bundle

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
